a12998e5e81892fb204fc3818c1704ee22413dbd
RusticPotato <battaglmb@gmail.com>
2024-02-24 13:03:49 -0500

    cleanup and moving add out to data

--- mysql/docker-entrypoint-initdb.d/01_tables.sql
+++ mysql/docker-entrypoint-initdb.d/01_tables.sql
@@ -125,14 +125,6 @@ CREATE TABLE `playerHiscoreData` (
 );
 CREATE TRIGGER `hiscore_date_OnInsert` BEFORE INSERT ON `playerHiscoreData` FOR EACH ROW SET new.ts_date = DATE(new.timestamp);
 
-start transaction;
-
-drop table if exists skills;
-drop table if exists activities;
-drop table if exists player_skills;
-drop table if exists player_activities;
-drop table if exists scraper_data;
-
 # done
 CREATE TABLE scraper_data (
   scraper_id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -147,112 +139,12 @@ CREATE TABLE skills (
   skill_name VARCHAR(50) NOT NULL,
   UNIQUE KEY unique_skill_name (skill_name)
 );
-INSERT INTO skills (skill_name) VALUES 
-	('total'), 
-	('attack'), 
-	('defence'), 
-	('strength'), 
-	('hitpoints'), 
-	('ranged'), 
-	('prayer'), 
-	('magic'), 
-	('cooking'), 
-	('woodcutting'), 
-	('fletching'), 
-	('fishing'), 
-	('firemaking'), 
-	('crafting'), 
-	('smithing'), 
-	('mining'), 
-	('herblore'), 
-	('agility'), 
-	('thieving'), 
-	('slayer'), 
-	('farming'), 
-	('runecraft'), 
-	('hunter'), 
-	('construction')
-;
-
 CREATE TABLE activities (
   activity_id TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, # < 255
   activity_name VARCHAR(50) NOT NULL,
   UNIQUE KEY unique_activity_name (activity_name)
 );
 
-INSERT INTO activities (activity_name) VALUES 
-	('abyssal_sire'),
-	('alchemical_hydra'),
-	('artio'),
-	('barrows_chests'),
-	('bounty_hunter_hunter'),
-	('bounty_hunter_rogue'),
-	('bryophyta'),
-	('callisto'),
-	('calvarion'),
-	('cerberus'),
-	('chambers_of_xeric'),
-	('chambers_of_xeric_challenge_mode'),
-	('chaos_elemental'),
-	('chaos_fanatic'),
-	('commander_zilyana'),
-	('corporeal_beast'),
-	('crazy_archaeologist'),
-	('cs_all'),
-	('cs_beginner'),
-	('cs_easy'),
-	('cs_elite'),
-	('cs_hard'),
-	('cs_master'),
-	('cs_medium'),
-	('dagannoth_prime'),
-	('dagannoth_rex'),
-	('dagannoth_supreme'),
-	('deranged_archaeologist'),
-	('duke_sucellus'),
-	('general_graardor'),
-	('giant_mole'),
-	('grotesque_guardians'),
-	('hespori'),
-	('kalphite_queen'),
-	('king_black_dragon'),
-	('kraken'),
-	('kreearra'),
-	('kril_tsutsaroth'),
-	('league'),
-	('lms_rank'),
-	('mimic'),
-	('nex'),
-	('nightmare'),
-	('obor'),
-	('phantom_muspah'),
-	('phosanis_nightmare'),
-	('rifts_closed'),
-	('sarachnis'),
-	('scorpia'),
-	('skotizo'),
-	('soul_wars_zeal'),
-	('spindel'),
-	('tempoross'),
-	('the_corrupted_gauntlet'),
-	('the_gauntlet'),
-	('the_leviathan'),
-	('the_whisperer'),
-	('theatre_of_blood'),
-	('theatre_of_blood_hard'),
-	('thermonuclear_smoke_devil'),
-	('tombs_of_amascut'),
-	('tombs_of_amascut_expert'),
-	('tzkal_zuk'),
-	('tztok_jad'),
-	('vardorvis'),
-	('venenatis'),
-	('vetion'),
-	('vorkath'),
-	('wintertodt'),
-	('zalcano'),
-	('zulrah')
-;
 
 CREATE TABLE player_skills (
   scraper_id BIGINT UNSIGNED NOT NULL,
