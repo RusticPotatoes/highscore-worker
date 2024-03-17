d7fc824268bcbf80366dfd9b32817a06c97a77ce
extreme4all <40169115+extreme4all@users.noreply.github.com>
2024-03-17 19:05:34 +0100

    bug fix,

--- src/app/schemas/input/player.py
+++ src/app/schemas/input/player.py
@@ -13,9 +13,5 @@ class Player(BaseModel):
     confirmed_player: Optional[bool] = None
     label_id: Optional[int] = None
     label_jagex: Optional[int] = None
-    ironman: Optional[bool] = None
-    hardcore_ironman: Optional[bool] = None
-    ultimate_ironman: Optional[bool] = None
-    normalized_name: Optional[bool] = None
     created_at: Optional[str] = None
     updated_at: Optional[str] = None
